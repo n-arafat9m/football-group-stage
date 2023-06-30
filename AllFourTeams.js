87b493e6f65f755b293b7bf2085ebdfa1819afb9
Muhammad Arafat <arafat9mat@gmail.com>
2023-07-01 00:30:16 +0300

    I'm not sure how make sense of the results anymore

--- AllFourTeams.js
+++ AllFourTeams.js
@@ -1,9 +1,11 @@
+import { per } from 'percom';
+
 const m = new Map();
 const W = 3, L = 0, D = 1;
 
 var fun = function(team1, team2, team3, team4, matchday) {
     if (matchday == 6) {
-        state = `${team1},${team2},${team3},${team4}`
+        let state = `${team1},${team2},${team3},${team4}`
         if(!m.get(state)) {
             m.set(state, {val: 0});
         }
@@ -58,13 +60,54 @@ fun(0, 0, 0, 0, 0);
 
 let s = 0;
 
-console.log('The number of possibilities for each point distribution:')
+//console.log('The number of possibilities for each point distribution:')
 
-m.forEach((v, k) => {
-    console.log(`${k}: ${v.val}`);
-    s += v.val;
+//m.forEach((v, k) => {
+//    console.log(`${k}: ${v.val}`);
+//    s += v.val;
+//})
+
+//console.log(`The total number of possibilities is ${s}, which is exactly nine to the sixth (there are nine different possibilities on each matchday)`)
+
+//console.log(`There are ${m.get('8,8,8,8').val} ways every team can end up with eight points by the end of the stage`)
+
+let keys = [...m.keys()];
+let newM = new Map();
+
+keys.forEach((key) => {
+    let arr = key.split(',')
+    arr = arr.map(el => parseInt(el))
+    arr.sort((a, b) => a - b)
+    let [a, b, c, d] = arr
+    let newKey = `${a},${b},${c},${d}`
+    if (!newM.get(newKey)) {
+        newM.set(newKey, {val: 0});
+    }
+    let pers = per(arr, arr.length)
+    pers.forEach((el) => {
+        const [a, b, c, d] = el
+        const s = `${a},${b},${c},${d}`
+        if(m.get(s).val) {
+            newM.get(newKey).val += m.get(s).val;
+            m.get(s).val = 0;
+        }
+    })
 })
 
-console.log(`The total number of possibilities is ${s}, which is exactly nine to the sixth (there are nine different possibilities on each matchday)`)
+let ss = 0;
+newM.forEach((v, k) => {
+    console.log(`${k}: ${v.val}`)
+    ss += v.val;
+})
+console.log(`total: ${ss}`)
+console.log(newM.get('8,8,8,8'))
 
-console.log(`There is ${m.get('8,8,8,8').val} ways every team can end up with eight points by the end of the stage`)
+let mx = { a: '', b: -1 }
+for(let i of newM) {
+    let [a, b] = i
+    mx.b = Math.max(mx.b, b.val)
+    if (mx.b == b.val) mx.a += `|${a}`
+}
+mx.a = mx.a.split('|')
+mx.a.forEach(e => console.log(e))
+console.log(mx.b)
